79e2e64512b9b45aafc4042b6c89cf511a6fcb3a
CarlosFredGIT <carlosk49@hotmail.com>
2021-08-11 14:16:41 -0300

    V1

---
+++ ProjetoLivraria.Services/Controllers/LivrosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLivraria.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post() //Inserir
+        {
+            return Ok(); // Provisório
+        }
+
+        [HttpPut]
+        public IActionResult Put() //Alterar
+        {
+            return Ok(); // Provisório
+        }
+
+        [HttpDelete]
+        public IActionResult Delete() //Deletar
+        {
+            return Ok(); // Provisório
+        }
+
+        [HttpGet]
+        public IActionResult Get() //Consultar
+        {
+            return Ok(); // Provisório
+        }  
+    }
+}
